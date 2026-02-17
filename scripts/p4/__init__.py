3af45acfe8bdbe7831c218b0204acb2a7f65f878
Di Zhao <virtuedizhao@gmail.com>
2026-02-18 10:34:51 +1300

    feat: Add initial implementation of Protocol 4 package and sampler

---
+++ scripts/p4/__init__.py
@@ -0,0 +1,1 @@
+# Protocol 4 Package: Multi-image Query (Fixed Gallery, Variable Query Size)
---
+++ scripts/p4/sampler.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+from collections import defaultdict
+from typing import Any, Dict, List, Optional, Set
+
+
+class MultiImageBatchSampler:
+    def __init__(
+        
+    ):
+        
